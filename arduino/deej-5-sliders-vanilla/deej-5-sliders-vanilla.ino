ebf321e7615e9e9e0b709f4ab7373c3c6e65beeb
Amadeo Ramirez <ramirez.amadeo1@gmail.com>
2025-03-17 22:01:19 -0700

    basic mutes

--- arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
+++ arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
@@ -1,11 +1,20 @@
-const int NUM_SLIDERS = 5;
-const int analogInputs[NUM_SLIDERS] = {A0, A1, A2, A3, A4};
+const int NUM_SLIDERS = 4;
+const int analogInputs[NUM_SLIDERS] = {A0, A1, A2, A3};
+const int buttonInputs[NUM_SLIDERS] = {7,6,5,4};
+const int ledOutputs[NUM_SLIDERS] = {11,10,9,8};
+
 
 int analogSliderValues[NUM_SLIDERS];
+bool sliderMute[NUM_SLIDERS];
+bool currentRead[NUM_SLIDERS];
+bool buttonPressed = false;
 
 void setup() { 
   for (int i = 0; i < NUM_SLIDERS; i++) {
     pinMode(analogInputs[i], INPUT);
+    pinMode(buttonInputs[i], INPUT);
+    pinMode(ledOutputs[i], OUTPUT);
+    sliderMute[i] = false;
   }
 
   Serial.begin(9600);
@@ -13,8 +22,31 @@ void setup() {
 
 void loop() {
   updateSliderValues();
+
+  // move to -> updateMuteValues()
+  bool buttonCheck = false;
+  for (int i = 0; i < NUM_SLIDERS; i++) {
+    currentRead[i] = digitalRead(buttonInputs[i]);
+    if (digitalRead(buttonInputs[i]) == true) {
+      buttonCheck = true;
+    }
+  }
+
+  if (buttonCheck && !buttonPressed) {
+    for (int i = 0; i < NUM_SLIDERS; i++) {
+      if (currentRead[i]) {
+        sliderMute[i] = !sliderMute[i];
+      }
+      digitalWrite(ledOutputs[i], sliderMute[i]);
+    }
+    buttonPressed = true;
+  } else if (!buttonCheck && buttonPressed) {
+    buttonPressed = false;
+  }
+
   sendSliderValues(); // Actually send data (all the time)
   // printSliderValues(); // For debug
+  myDebug();
   delay(10);
 }
 
@@ -28,6 +60,7 @@ void sendSliderValues() {
   String builtString = String("");
 
   for (int i = 0; i < NUM_SLIDERS; i++) {
+    analogSliderValues[i] = sliderMute[i] ? 0 : analogSliderValues[i];
     builtString += String((int)analogSliderValues[i]);
 
     if (i < NUM_SLIDERS - 1) {
@@ -36,6 +69,10 @@ void sendSliderValues() {
   }
   
   Serial.println(builtString);
+}
+
+void myDebug () {
+
 }
 
 void printSliderValues() {
